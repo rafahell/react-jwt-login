d345be92892222a0842908fb3a6795025573527b
Rafael Ribeiro <220rrs@gmail.com>
2022-11-04 12:33:54 +0000

    remove key

--- django_jwt/config/settings.py
+++ django_jwt/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7_-lv^7@!%%^jl9#p7in*!&f3fx6!bk*z5-9qab$epq4^*@9%p"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
